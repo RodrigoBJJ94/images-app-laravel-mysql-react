8a775945bb7a42dd142315b05b106ea3c84aa7b3
Rodrigo Brentano <rodrigobjj24@gmail.com>
2022-04-25 02:52:49 -0300

    Version 1.04

--- client/src/components/Data/Data.js
+++ client/src/components/Data/Data.js
@@ -6,7 +6,10 @@ export default function Data({ list }) {
         <div className="divMap">
             {list.map((item, index) => (
                 <div key={index} data={item} className="divData">
-                    <img src={item.url} className="image" />
+                    <img
+                        src={item.url}
+                        alt={item.subtitle}
+                        className="image" />
                     <p className="subtitle">{item.subtitle}</p>
                 </div>
             ))}
--- server/routes/api.php
+++ server/routes/api.php
@@ -5,17 +5,6 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Storage;
 use Intervention\Image\Facades\Image;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
 Route::post('upload', function (Request $request) {
     $subtitle = $request->get('subtitle');
     $image = $request->file('image');
